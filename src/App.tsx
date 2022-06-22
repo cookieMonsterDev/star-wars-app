0e160c86cd69539303cf845a511df743d0518e95
It-is-a-piece-of-cake <mykhailo.toporkov@gmail.com>
2022-06-22 22:06:17 +0300

    feat: first fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const URL = 'https://swapi.dev/api/planets/1/'
+
+const getData = () => {
+  fetch(URL)
+    .then(response => {
+      return response.json();
+  }).then(json => console.log(json))
+};
+
+
+
 function App() {
   return (
     <div>
-      Star Wars
+      <button onClick={getData}>Star Wars</button>
     </div>
   );
 }
