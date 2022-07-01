bb4d3a2bf9e789968b56334bff6986144cf62852
It-is-a-piece-of-cake <mykhailo.toporkov@gmail.com>
2022-07-01 20:45:33 +0300

    feat: update slider to tree elements

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -2,42 +2,46 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  margin: auto;
-  width: 50em;
+  position: relative;
   height: 30em;
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
-  background-color: gray;
 `;
 
-const Form = styled.div`
-  margin: auto;
-  justify-content: center;
-  align-items: center;
-  width: 10em;
-  height: 20em;
-  border-width: 2em;
+const IMG = styled.img`
+  width: 300px;
+  height: 150px;
+  border-radius: 10px;
+  border-style: solid;
   border-color: black;
-  background-color: orange;
+  border-width: 0.2em;
 `;
+
+const Slide = styled.div`
+`;
+
 
 const Data = [
   {
-    id: '1'
+    image:
+      'https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
   },
   {
-    id: '2'
+    image:
+      'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80'
   },
   {
-    id: '3'
+    image:
+      'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
   },
   {
-    id: '4'
+    image:
+      'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
   },
   {
-    id: '5'
+    image:
+      'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
   }
 ];
 
@@ -55,18 +59,44 @@ export const Slider = () => {
     setSlide( slide === 0 ? Data.length - 1 : slide - 1);
   };
 
-  console.log(slide);
-
   return (
     <Container>
       <button onClick={prevSlide}> Prev </button>
+
+
+      { Data.map((sth, index) => {
+          return ( 
+            <>
+              <Slide>
+                { (slide === 0 
+                  ? index === Data.length - 1 
+                  : index === slide - 1) 
+                  && (<IMG src={sth.image} />)}
+              </Slide>
+            </>
+        )})};
+
         { Data.map((sth, index) => {
-          return (
-          <div className={index === slide ? 'slider active' : 'slide' } key={index}>
-            { index === slide && (<Form>{ sth.id }</Form>)}
-          </div>
-    
-        )})}
+          return ( 
+            <>
+              <Slide>
+                { index === slide && (<IMG src={sth.image} />)}                
+              </Slide>
+            </>
+        )})};
+
+        { Data.map((sth, index) => {
+          return ( 
+            <>
+              <Slide>
+                { (slide === Data.length - 1 
+                  ? index === 0 
+                  : index === slide + 1) 
+                  && (<IMG src={sth.image} />)}                
+              </Slide>
+            </>
+        )})};
+
       <button onClick={nextSlide}> Next </button>
     </Container>
   )  
