04f9d61b98058442a70b03e92829c2d42ae0579a
It-is-a-piece-of-cake <mykhailo.toporkov@gmail.com>
2022-07-02 19:02:58 +0300

    feat: update carusel design

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,76 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
+import './CSS.css'
 
 const Container = styled.div`
-  height: 60em;
-  display: flex;
+  margin: auto;
+  width: 50em;
+  height: 50em;
+  margin: auto;
   justify-content: center;
   align-items: center;
-`;
-
-const IMG = styled.img`
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-  border-style: solid;
-  border-color: black;
-  border-width: 0.2em;
-`;
-
-const animation = keyframes`
-  from {
-    opacity: 0.5;
-  }
-
-  to {
-    topacity: 1;
-  }
-`;
-
-const MainSlide = styled.div`
-  width: 35em;
-  height: 40em;
-  z-index: 1;
-  animation: ${animation} 0.5s;
-`;
-
-const RightSlide = styled.div`
-  position: relative;
-  width: 25em;
-  height: 34em;
-  opacity: 0.5;
-  right: 5em;
-  animation: ${animation} 0.5s;
-`; 
-
-const LeftSlide = styled.div`
-  position: relative; 
-  width: 25em;
-  height: 34em;
-  opacity: 0.5;
-  left: 5em;
-  animation: ${animation} 0.5s;
-`;
-
+`
 
 const Data = [
   {
+    id: '1',
     image:
       'https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
   },
   {
+    id: '2',
     image:
       'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80'
   },
   {
+    id: '3',
     image:
       'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
   },
   {
+    id: '4',
     image:
       'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
   },
   {
+    id: '5',
     image:
       'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
   }
@@ -80,54 +43,57 @@ const Data = [
 
 export const Slider = () => {
 
-  const [slide, setSlide] = useState(0);
+  const [index, setIndex] = useState(0);
+
+  const mod = (n: any, m: any) => {
+    let result = n % m;
+
+    // Return a positive value
+    return result >= 0 ? result : result + m;
+  };
+
 
   const nextSlide = () => {
-    setSlide( slide === Data.length - 1 ? 0 : slide + 1);
+    setIndex( index === Data.length - 1 ? 0 : index + 1);
   };
 
   const prevSlide = () => {
-    setSlide( slide === 0 ? Data.length - 1 : slide - 1);
+    setIndex( index === 0 ? Data.length - 1 : index - 1);
   };
 
+
   return (
-    <Container>
+    <>
       <button onClick={prevSlide}> Prev </button>
-
-      { Data.map((sth, index) => {
-          return ( 
-            <>
-              { (slide === 0 
-                ? index === Data.length - 1 
-                : index === slide - 1) 
-                && (<LeftSlide onClick={prevSlide}>
-                      <IMG src={sth.image} />
-                    </LeftSlide>)}
-            </>
-        )})};
-
-        { Data.map((sth, index) => {
-          return ( 
-            <>
-              { index === slide && (<MainSlide>
-                <IMG src={sth.image} />
-              </MainSlide>)}                
-            </>
-        )})};
-
-        { Data.map((sth, index) => {
-          return ( 
-            <>
-              { (slide === Data.length - 1 
-                ? index === 0 
-                : index === slide + 1) 
-                && (<RightSlide onClick={nextSlide}>
-                      <IMG src={sth.image} />
-                    </RightSlide>)}                
-            </>
-        )})};
-
       <button onClick={nextSlide}> Next </button>
+    <Container>
+    <div className="carousel">
+        {Data.map((item, i) => {
+          const indexLeft = mod(index - 1, Data.length);
+          const indexRight = mod(index + 1, Data.length);
+
+          let className = "card";
+
+          if (i === index) {
+            className = "card card--active";
+          } else if (i === indexRight) {
+            className = "card card--right";
+          } else if (i === indexLeft) {
+            className = "card card--left";
+          } else className = "card";
+
+          return (
+            <img
+              key={item.id}
+              className={className}
+              src={item.image}
+              alt="Comic"
+            ></img>
+          );
+        })}
+      </div>
     </Container>
+    
+    </>
   )  
 };
