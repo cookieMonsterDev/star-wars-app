34ad2402aa3a73cffbc3e811f21488267bddf3ed
It-is-a-piece-of-cake <mykhailo.toporkov@gmail.com>
2022-07-28 20:41:37 +0300

    feat: add labels

--- src/assets/images/starships/starships.ts
+++ src/assets/images/starships/starships.ts
@@ -12,51 +12,61 @@ import rebeltransport from './rebeltransport.jpg';
 export const Starships = [
   {
     id: '1',
+    fraction: 'rebels',
     name: 'cr90corvette',
     url: cr90corvette
   },
   {
     id: '2',
+    fraction: 'empire',
     name: 'stardestroyer',
     url: stardestroyer
   },
   {
     id: '3',
+    fraction: 'empire',
     name: 'sentinel-classlandingcraft',
     url: sentinelclasslandingcraft
   },
   {
     id: '4',
+    fraction: 'empire',
     name: 'deathstar',
     url: deathstar
   },
   {
     id: '4',
+    fraction: 'rebels',
     name: 'millenniumfalcon',
     url: millenniumfalcon
   },
   {
     id: '6',
+    fraction: 'rebels',
     name: 'y-wing',
     url: ywing
   },
   {
     id: '7',
+    fraction: 'rebels',
     name: 'x-wing',
     url: xwing
   },
   {
     id: '8',
+    fraction: 'empire',
     name: 'tieadvancedx1',
     url: tieadvancedx1
   },
   {
     id: '9',
+    fraction: 'empire',
     name: 'executor',
     url: executor
   },
   {
     id: '10',
+    fraction: 'rebels',
     name: 'rebeltransport',
     url: rebeltransport
   },
--- src/components/Starships/components/HoverTabs.tsx
+++ src/components/Starships/components/HoverTabs.tsx
@@ -3,13 +3,20 @@ import { useRouteMatch, useHistory } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 import { getData, Responce } from '../../../typescript/getData';
 import { Starships as images } from '../../../assets/images/starships/starships';
+import rebels from '../../../assets/images/blazon/rebels.jpg'
+import empire from '../../../assets/images/blazon/empire.jpg'
+
 
 interface StarshipProps {
   imageUrl?: any
 };
 
-interface SVGProps {
+interface LabelProps {
   name?: string
+};
+
+interface BlazonProps {
+  blazonColor?: string
 };
 
 const Animation = keyframes`
@@ -36,81 +43,14 @@ const Container = styled.div`
   animation: ${Animation} 700ms;
 `;
 
-// const Starship = styled.div<StarshipProps>`
-//   position: relative;
-//   display: flex;
-//   justify-content: center;
-//   margin: 0.5em;
-//   height: 100%;
-//   flex: 0.5;
-//   border-radius: 3em;
-//   transition: all 500ms;
-//   background-image: url(${(props => props.imageUrl)});
-//   background-position: center; 
-//   background-repeat: no-repeat;
-//   background-size: cover;
-
-//   > svg {
-//     position: absolute;
-//     height: 50px;
-//     width: 50px;
-//     bottom: 0.5em;
-//     left: 50%;
-//     border-radius: 50%;
-//     background-color: #bbb;
-//     transform: translate(-50%, -50%);
-//     transition: all 500ms;
-
-//     &:before {
-//       content: '';
-//       position: absolute;
-//       height: 50px;
-//       width: 50px;
-//       bottom: 5em;
-//       left: 50%;
-//       border-radius: 50%;
-//       background-color: red;
-//       transform: translate(-50%, -50%);
-//     }
-//   }
-
-//   > h1 {
-//     position: absolute;
-//     bottom: 8em; 
-//     width: 12em;
-//     color: white;
-//     transform: rotate(-90deg);
-//   }
-
-//   &:hover {
-//     flex: 7;
-//     transition: all 500ms;
-
-//     > svg {
-//       left: 8%;
-//     }
-     
-//     > h1 {
-//       left: 16%;
-//       bottom: 0.5em;
-//       transform: rotate(0deg);
-//       transition: all 500ms;
-//     }
-//   }
-// `;
-
-const SVG = styled.div<SVGProps>`
+const Label = styled.div<LabelProps>`
   position: absolute;
-  height: 50px;
-  width: 50px;
+  height: 5em;
+  width: 5em;
   bottom: 2em;
   left: 50%;
   transform: translate(-50%, 0);
-  background-color: #bbb;
   transition: all 500ms;
-
-  font-size: 2em;
-  font-weight: 600;
 
   &:before {
     content: '${(props => props.name)}';
@@ -119,10 +59,29 @@ const SVG = styled.div<SVGProps>`
     width: 20em;
     bottom: 2em;
     left: 50%;
-    color: #FFFFFF;
+    color: #d8d8d8;
+    font-size: 2em;
+    font-weight: 600;
+    font-family: 'Invisible', sans-serif;
     transform-origin: 0 50%;
     transform: rotate(-90deg);
   }
+ `;
+
+const Color = {
+  red: 'invert(37%) sepia(91%) saturate(4716%) hue-rotate(343deg) brightness(101%) contrast(102%)',
+  blue: 'invert(48%) sepia(38%) saturate(1306%) hue-rotate(172deg) brightness(87%) contrast(82%)'
+};
+
+const Blazon = styled.img<BlazonProps>`
+  position: absolute;
+  height: 5em;
+  width: 5em;
+  bottom: 2em;
+  left: 50%;
+  filter: ${(props => props.blazonColor)};
+  transform: translate(-50%, 0);
+  transition: all 500ms;
  `;
 
  const Starship = styled.div<StarshipProps>`
@@ -139,14 +98,24 @@ const SVG = styled.div<SVGProps>`
   background-repeat: no-repeat;
   background-size: cover;
 
+  > h1 {
+    color: yellow;
+  }
+
   &:hover {
     flex: 7;
     transition: all 500ms;
 
-    ${SVG} {
+    ${Label} {
       left: 2em;
       transform-origin: center;
       transform: rotate(90deg);
+    }
+
+    ${Blazon} {
+      left: 2em;
+      transform-origin: center;
+      transform: rotate(0deg);
     }
   }
 `;
@@ -177,11 +146,13 @@ const HoverTabs = () => {
       {data.map((item: Responce, key: number) => {
 
         let url: any;
+        let fraction: any;
 
         images.map((i) => {
           const name = item.name?.toLocaleLowerCase().replace(/\s/g, '');
           if (i.name === name) {
-            return url = i.url;
+            url = i.url;
+            fraction = i.fraction;
           }
         });
 
@@ -191,7 +162,11 @@ const HoverTabs = () => {
             imageUrl={url}
             onClick={() => handleClick(item)}
           >
-          <SVG name={item.name}></SVG>
+            <Label name={item.name}/>
+            <Blazon 
+              src={ fraction === 'rebels' ? rebels : empire}
+              blazonColor={ fraction === 'rebels' ? Color.red : Color.blue}
+            />
           </Starship>
           )
         })}
